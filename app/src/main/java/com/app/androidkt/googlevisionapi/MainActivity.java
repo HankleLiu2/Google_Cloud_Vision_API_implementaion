037c643d163d130021d0211e87f498ade0fca3b8
HankleLiu2 <mr.liuhankle@gmail.com>
2019-01-20 10:07:36 -0500

    Changed API

--- app/src/main/java/com/app/androidkt/googlevisionapi/MainActivity.java
+++ app/src/main/java/com/app/androidkt/googlevisionapi/MainActivity.java
@@ -54,7 +54,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     private static final int RECORD_REQUEST_CODE = 101;
     private static final int CAMERA_REQUEST_CODE = 102;
 
-    private static final String CLOUD_VISION_API_KEY = "AIzaSyA40SjWGfxwULJNqVjkVvw3rSgWLNTc4m0";
+    private static final String CLOUD_VISION_API_KEY = "AIzaSyBYc7BxcH4RCg1B0VEpqDNCEaZHxx3EEHQ";
 
     @BindView(R.id.takePicture)
     Button takePicture;
